e15a3545c7b663b35ff371290cd05bd0f4fde9ae
Gabriela Verza <84354639+gabrielaverza@users.noreply.github.com>
2024-09-11 13:22:44 -0300

    new flow

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.search_product, name='search_product'),
-    path('add/', views.add_product, name='add_product'),
+    path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', views.custom_logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('initial/', views.initial, name='initial'),
+    path('search_product/', views.search_product, name='search_product'),
+    path('add_product/', views.add_product, name='add_product'),
 ]
